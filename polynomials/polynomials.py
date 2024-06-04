8eaf5ac742ad031a5289558d8a843db23b3dc600
Paul Mannix <pmannix@unice.fr>
2024-06-04 15:07:10 +0100

    Completed exercises 3.4,3.5

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,5 +1,12 @@
 from numbers import Number
+import numpy as np
 
+def derivative(f):
+
+    if isinstance(f,Polynomial):
+        return f.dx() 
+    else:
+        return NotImplemented
 
 class Polynomial:
 
@@ -50,3 +57,104 @@ class Polynomial:
 
     def __radd__(self, other):
         return self + other
+
+
+    def __sub__(self, other):
+
+        if isinstance(other, Polynomial):
+            # Work out how many coefficients they have in common
+            common = min(self.degree(), other.degree()) + 1
+            
+            # subtract the common coefficients
+            coefs  = tuple(a - b for a, b in zip(self.coefficients,other.coefficients))
+            
+            # append the higher degree coefficients
+            coefs += self.coefficients[common:] + tuple(-b for b in other.coefficients[common:])
+            
+            return Polynomial(coefs)
+            
+        elif isinstance(other, Number):
+            # subtract the number from the lowest cofficient + append other coefficients
+            print(Polynomial((self.coefficients[0] - other,) + self.coefficients[1:]))
+            return Polynomial((self.coefficients[0] - other,) + self.coefficients[1:])
+            
+        else:
+            return NotImplemented
+
+    def __rsub__(self, other):
+        
+        if isinstance(other, Polynomial):
+            # Work out how many coefficients they have in common
+            common = min(self.degree(), other.degree()) + 1
+            
+            # subtract the common coefficients
+            coefs  = tuple(-a + b for a, b in zip(self.coefficients,other.coefficients))
+            
+            # append the higher degree coefficients
+            coefs += tuple(-a for a in self.coefficients[common:]) + self.coefficients[common:]
+            
+            return Polynomial(coefs)
+            
+        elif isinstance(other, Number):
+            
+            # subtract the number from the lowest cofficient + append other coefficients
+            return Polynomial((other - self.coefficients[0],) + tuple(-a for a in self.coefficients[1:]) )
+            
+        else:
+            return NotImplemented
+    
+    def __mul__(self,other):
+
+        if isinstance(other, Polynomial):
+
+            coefs = np.zeros(self.degree() + other.degree() + 1)
+            for i,a in enumerate(self.coefficients):    
+                for j,b in enumerate(other.coefficients):
+                    coefs[i+j] += a*b
+
+            return Polynomial(tuple(coefs))
+
+        elif isinstance(other, Number):
+
+            coefs = tuple(other*a for a in self.coefficients)
+            return Polynomial(coefs)
+
+        else:
+            return NotImplemented
+
+    def __rmul__(self,other):
+        return self * other        
+
+    def __pow__(self,other):
+
+        if   other == 0:
+            return Polynomial(tuple(1))
+        
+        else:
+            fp = Polynomial(self.coefficients)
+            for i in range(other-1):
+                fp = self * fp
+        return fp
+    
+    def __call__(self,x):
+
+        f = 0.
+        for i,a in enumerate(self.coefficients):
+            f+= a * x**i
+        
+        return f
+
+    def dx(self):
+
+        if self.degree() == 0:
+            return Polynomial((0,))
+        else:
+            coefs = np.zeros(self.degree())
+            for i,a in enumerate(self.coefficients):
+                
+                if i==1:
+                    coefs[0] = a
+                elif (i>1) and (i != 0):
+                    coefs[i-1] = a*i
+            
+            return Polynomial(tuple(coefs))
